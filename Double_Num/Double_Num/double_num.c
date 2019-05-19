48ecc82fa9c46ad76d353d525f6b10f7b7a94d59
Rolf Baitinger <rolf@baitinger.info>
2019-05-19 18:54:22 +0200

    Ein

---
+++ Double_Num/Double_Num/double_num.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#pragma warning(disable : 4996)
+
+int main(int argc, char* argv[])
+{
+	FILE* in = stdin;
+	char buffer[80 + 1];
+	long long number;
+
+	if (in == NULL)
+	{
+		perror("Error while opening the file.\n");
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		if (argc == 2)
+		{
+			in = fopen(argv[1], "r");
+			if (!in)
+			{
+				exit(EXIT_FAILURE);
+			}
+		}
+		while (fgets(buffer, 80, in))
+		{
+			number = 2 * atoll(buffer);
+			printf("%lld\n", number);
+		}
+		if (argc == 2)
+		{
+			fclose(in);
+		}
+		exit(EXIT_SUCCESS);
+	}
+}
